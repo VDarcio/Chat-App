8ee2b348127664a0e754ef2819f68cd30ce163be
Victor Alvarenga <alvarengavictor73@gmail.com>
2021-10-09 20:39:46 +0100

    initial commit

---
+++ Flash Chat iOS13/Messsage.swift
@@ -0,0 +1,15 @@
+//
+//  Messsage.swift
+//  Flash Chat iOS13
+//
+//  Created by VD on 10/06/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Messsage {
+    let sender : String //1@2.com
+    let body: String// ola
+    
+}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by VD on 19/06/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    @IBOutlet weak var MessageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        MessageBubble.layer.cornerRadius=MessageBubble.frame.size.height/5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
